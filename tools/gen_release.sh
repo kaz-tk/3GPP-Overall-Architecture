022ee4d740b8e7b90e8cd98aa425e380be9e3d91
Kazushige Takeuchi <Kazushige.Takeuchi@sony.com>
2021-11-14 06:19:26 +0900

    add || : for convert command

--- tools/gen_release.sh
+++ tools/gen_release.sh
@@ -20,7 +20,7 @@ done
 echo "PDF to image phase"
 ls
 sudo cp tools/policy.xml /etc/ImageMagick-6/ || :
-convert -density 400 -resize 3000^ ${FNAME}.pdf ${FNAME}.png
-convert -density 400 -resize 3000^ ${FNAME}.pdf ${FNAME}.jpg
+convert -density 400 -resize 3000^ ${FNAME}.pdf ${FNAME}.png  || :
+convert -density 400 -resize 3000^ ${FNAME}.pdf ${FNAME}.jpg  || :
 
 
